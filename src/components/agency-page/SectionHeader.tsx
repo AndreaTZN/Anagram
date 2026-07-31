b1777591ce7d247c0f8f47110224598a8efadda4
AlexTznDev <alex.tuysuzian91@gmail.com>
2026-07-31 10:26:48 +0200

    Update

--- src/components/agency-page/SectionHeader.tsx
+++ src/components/agency-page/SectionHeader.tsx
@@ -9,7 +9,7 @@ export default function SectionHeader({ eyebrow, heading }: Props) {
       <span className="text-[#7e7e7e] text-sm leading-[0.9] shrink-0">
         {eyebrow}
       </span>
-      <h2 className="text-[#0c0c0c] text-2xl leading-[1.1] tracking-[-0.02em] text-left pl-8 mt-6 max-[766px]:pl-0">
+      <h2 className="text-[#0c0c0c] text-2xl leading-[1.1] tracking-[-0.02em] text-left pl-8 mt-6 max-[766px]:pl-0 max-[766px]:mt-4">
         {heading}
       </h2>
     </div>
